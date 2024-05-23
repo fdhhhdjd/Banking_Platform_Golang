5f3e63e3721fbb4c609f412393f444705f799676
fdhhhdjd <nguyentientai10@gmail.com>
2024-05-23 09:45:55 +0700

    feat: validator create user

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -8,12 +8,19 @@ import (
 	"github.com/fdhhhdjd/Banking_Platform_Golang/internals/auth"
 	"github.com/fdhhhdjd/Banking_Platform_Golang/internals/constants"
 	"github.com/fdhhhdjd/Banking_Platform_Golang/internals/db"
+	"github.com/fdhhhdjd/Banking_Platform_Golang/internals/val"
 	"github.com/fdhhhdjd/Banking_Platform_Golang/pb"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (server *SimpleBankServer) CreateUser(c context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	violations := ValidateCreateUserRequest(req)
+	if violations != nil {
+		return nil, InvalidArgumentError(violations)
+	}
+
 	hashedPassword, err := auth.EncodePassword(req.GetPassword())
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
@@ -42,4 +49,24 @@ func (server *SimpleBankServer) CreateUser(c context.Context, req *pb.CreateUser
 		User: ConvertUser(user),
 	}
 	return rsp, nil
+}
+
+func ValidateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, FieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, FieldViolation("password", err))
+	}
+
+	if err := val.ValidateFullName(req.GetFullName()); err != nil {
+		violations = append(violations, FieldViolation("full_name", err))
+	}
+
+	if err := val.ValidateEmail(req.GetEmail()); err != nil {
+		violations = append(violations, FieldViolation("email", err))
+	}
+
+	return violations
 }
